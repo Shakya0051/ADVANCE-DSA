d93fec4ac43a5a543f2a24018bbd51b605af6b63
Saurabh Shakya <shakyasaurabh951@gmail.com>
2026-06-16 01:06:49 +0530

    F1

---
+++ Inorder(I).cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+
+
+    void inOrder(Node *root) {
+        stack<Node*> st;
+        Node* curr = root;
+        while (curr !=NULL || ! st.empty()) {
+            while (curr !=NULL) 
+            {
+                st.push(curr);
+                curr = curr->left;
+            
+            }
+            curr = st.top();
+            st.pop();
+            
+            cout<<curr->data << " ";
+            curr = curr-> right;
+        
+        } 
+
+    }
+
+};
